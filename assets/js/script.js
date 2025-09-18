4cf8022c29d05f57814bf49bb28b95b8d368cfb4
HRagunathan-03 <hragunathan@googlemail.com>
2025-09-18 03:35:40 +0100

    add CSS styles to display flower images correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,35 @@
 //Applies event listener to each game card.
 
 let buttonOne = document.getElementById("button-one");
-buttonOne.addEventListener("click", displayCardImage);
+buttonOne.addEventListener("click", displayTulipCard);
 
-function displayCardImage(e) {
+let buttonTwo = document.getElementById("button-two");
+buttonTwo.addEventListener("click", displaySunflowerCard);
+
+let buttonThree = document.getElementById("button-three");
+buttonThree.addEventListener("click", displaySunflowerCard);
+
+let buttonFour = document.getElementById("button-four");
+buttonFour.addEventListener("click", displayLotusCard);
+
+let buttonFive = document.getElementById("button-five");
+buttonFive.addEventListener("click", displayLotusCard);
+
+let buttonSix = document.getElementById("button-six");
+buttonSix.addEventListener("click", displayTulipCard);
+
+/**
+ * Displays the image for the game card.
+ */
+
+function displayTulipCard(e) {
     e.currentTarget.classList.toggle("btn--tulip");
+}
+
+function displaySunflowerCard(e) {
+    e.currentTarget.classList.toggle("btn--sunflower");
+}
+
+function displayLotusCard(e) {
+    e.currentTarget.classList.toggle("btn--lotus");
 }
